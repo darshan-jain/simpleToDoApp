f7c6c22aa07873890b35b84f90911eb3af810e0c
Darshan Jain <cb.en.u4cse19126@cb.students.amrita.edu>
2024-03-12 18:44:15 +0530

    final app

---
+++ index.js
@@ -0,0 +1,43 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if(inputBox.value === ''){
+        alert("Empty String");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
+    }
+    inputBox.value = '';
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    //check and uncheck task item
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    //delete task
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+
+function saveData() {
+    localStorage.setItem('data',listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+
+showTask();
